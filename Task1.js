bc5307b4f5db3c7e8f1d7429bc886de5b0334444
Asif-vai <mr3366533@gmail.com>
2025-07-26 21:49:41 +0600

    Solve Task-1: Calculate return money after buying fruits

---
+++ Task1.js
@@ -0,0 +1,8 @@
+const myMoney = 1000;
+const orangePrice = 409;
+const applePrice = 389;
+
+const totalCost = orangePrice + applePrice;
+const returnAmount = myMoney - totalCost;
+
+console.log("Return Amount:", returnAmount);
