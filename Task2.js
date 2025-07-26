b7d055ff7369affee758b0adb389744633c2f5e0
Asif-vai <mr3366533@gmail.com>
2025-07-26 22:11:49 +0600

    Solve Task-2: Calculate average marks of five subjects

---
+++ Task2.js
@@ -0,0 +1,10 @@
+let Mathematics = 75.25;
+let Biology = 65;
+let Chemistry = 80;
+let Physics = 35.45;
+let Bangla = 99.50;
+
+let total = Mathematics + Biology + Chemistry + Physics + Bangla;
+let average = total / 5;
+
+console.log(average.toFixed(2));
