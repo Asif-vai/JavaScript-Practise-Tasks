d8bc794855d25919c077a759ce7496287f76535c
Asif-vai <mr3366533@gmail.com>
2025-07-28 01:50:54 +0600

    Update Codes with Questions

--- Task4.js
+++ Task4.js
@@ -1,3 +1,15 @@
+// ### Task-4
+
+// What will be the result of the following codes:
+
+// **var a = isNaN(‘11’);**
+
+// **var a = isNaN(2-10);**
+
+// Explain your answers.
+
+
+
 var a = isNaN('11');
 console.log("isNaN('11') =", a); // Output: false
 
