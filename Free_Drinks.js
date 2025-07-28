77c8d22eeb0dd2c848e0ba5cc5fd3f319ac53637
Asif-vai <mr3366533@gmail.com>
2025-07-28 17:01:15 +0600

    Free drinks task solve

---
+++ Free_Drinks.js
@@ -0,0 +1,14 @@
+/***
+
+Free Drinks
+    - Burger more than 500tk: free Coke
+    - Else Coke: 30tk
+*/
+
+const Burger = 300;
+
+if(Burger > 500){
+    console.log('The Coke is free!');
+}else{
+    console.log('Coke is 30tk');
+}
