15eaec307bb01d4e0fa3cf1e88cf0352acfaed39
Asif-vai <mr3366533@gmail.com>
2025-07-28 17:39:48 +0600

    Add BMI calculator with health category using nested if-else

--- BMI_Calculator.js
+++ BMI_Calculator.js
@@ -11,3 +11,19 @@ Create a JavaScript program that calculates the Body Mass Index (BMI) and assign
     - Otherwise, you are obese.
 
 */
+
+const weight = 60; // kg
+const height = 1.70688; // meter
+
+const BMI = weight / (height ** 2);
+console.log("Your BMI is:", BMI.toFixed(1));
+
+if (BMI < 18.5) {
+    console.log('You are underweight');
+} else if (BMI >= 18.5 && BMI <= 24.9) {
+    console.log('You are normal');
+} else if (BMI >= 25 && BMI <= 29.9) {
+    console.log('You are overweight');
+} else {
+    console.log('You are obese');
+}
